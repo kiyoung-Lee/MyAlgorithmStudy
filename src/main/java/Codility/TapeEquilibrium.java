200334bba910b58c2edb9a07f1c7548b6d1b2fe3
kiyoung Lee <eevee300@gmail.com>
2017-11-13 22:38:45 +0900

    Add TapeEquilibrium Algorithm // Prefomance 향상 필요..

--- src/main/java/Codility/TapeEquilibrium.java
+++ src/main/java/Codility/TapeEquilibrium.java
@@ -5,8 +5,31 @@ public class TapeEquilibrium {
     public int solution(int[] A) {
         // write your code in Java SE 8
 
+        int arraySize = A.length;
+        int minSum = Integer.MAX_VALUE;
 
+        for(int i = 0; i < arraySize; i++){
+            int p = i + 1;
+            if(p < arraySize){
 
-        return 0;
+                int frontSum = 0;
+                int backSum = 0;
+
+                for(int q = 0; q < p; q++){
+                    frontSum += A[q];
+                }
+
+                for(int t = p; t < arraySize; t++){
+                    backSum += A[t];
+                }
+
+                int result = Math.abs(frontSum - backSum);
+                if(minSum > result){
+                    minSum = result;
+                }
+            }
+        }
+
+        return minSum;
     }
 }
--- src/test/java/Codility/TapeEquilibriumTest.java
+++ src/test/java/Codility/TapeEquilibriumTest.java
@@ -1,6 +1,7 @@
 package Codility;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,38 @@ public class TapeEquilibriumTest {
         solution = new TapeEquilibrium();
     }
 
+    @Test
+    public void solution_Test1() throws Exception {
+        int[] array = {3,1,2,4,3};
+        int result = solution.solution(array);
+        assertEquals(result, 1);
+    }
 
+    @Test
+    public void solution_2_Element_Test() throws Exception {
+        int[] array = {2,2};
+        int result = solution.solution(array);
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void solution_2_Element_Test2() throws Exception {
+        int[] array = {-2, 2};
+        int result = solution.solution(array);
+        assertEquals(result, 4);
+    }
+
+    @Test
+    public void solution_3_Element_Test1() throws Exception {
+        int[] array = {-1, 0 ,1};
+        int result = solution.solution(array);
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void solution_3_Element_Test2() throws Exception {
+        int[] array = {-3, -2, -1};
+        int result = solution.solution(array);
+        assertEquals(result, 0);
+    }
 }
