b498521ec46ac03267c92259ba98117cd44589f4
KiyoungLee <eevee300@gmail.com>
2017-12-05 23:16:41 +0900

    Add Fibonacci Solution

---
+++ src/test/java/Personal/FibonacciTest.java
@@ -0,0 +1,45 @@
+package Personal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+    private Fibonacci solution;
+
+    @Before
+    public void setUp(){
+        solution = new Fibonacci();
+    }
+
+    @Test
+    public void sample_Test() throws Exception {
+        int result = solution.solution(0);
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void sample_Test_1() throws Exception {
+        int result = solution.solution(1);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void sample_Test_2() throws Exception {
+        int result = solution.solution(2);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void sample_Test3() throws Exception {
+        int result = solution.solution(3);
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void sample_Test4() throws Exception {
+        int result = solution.solution(7);
+        assertEquals(result, 13);
+    }
+}
