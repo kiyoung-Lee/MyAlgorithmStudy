5ab942c6bf23f93355a2dbe7b3220f67986c87ec
KiyoungLee <eevee300@gmail.com>
2017-12-17 14:40:51 +0900

    Add PassingCar Solution

---
+++ src/main/java/Codility/PassingCars.java
@@ -0,0 +1,8 @@
+package Codility;
+
+public class PassingCars {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        return 0;
+    }
+}
---
+++ src/test/java/Codility/PassingCarsTest.java
@@ -0,0 +1,7 @@
+package Codility;
+
+import static org.junit.Assert.*;
+
+public class PassingCarsTest {
+
+}
