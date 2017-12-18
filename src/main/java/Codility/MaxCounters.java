800409d6f25d9718616239be0f933434585a9681
KiyoungLee <eevee300@gmail.com>
2017-12-19 00:35:53 +0900

    resolve MaxCounters Solution

--- src/main/java/Codility/MaxCounters.java
+++ src/main/java/Codility/MaxCounters.java
@@ -9,18 +9,28 @@ public class MaxCounters {
 
         int condition = N + 1;
         int currentMaxValue = 0;
+        int lastUpdate = 0;
         int [] result = new int[N];
 
         for(int i = 0; i < A.length; i++){
             if(A[i] == condition){
-                Arrays.fill(result, currentMaxValue);
+                lastUpdate = currentMaxValue;
             }else{
                 int position = A[i] - 1;
-                int value = result[position] + 1;
-                result[position] = value;
+                int value = result[position];
+                if(value < lastUpdate){
+                    result[position] = lastUpdate + 1;
+                }else
+                    result[position]++;
 
-                if(value > currentMaxValue)
-                    currentMaxValue = value;
+                if(result[position] > currentMaxValue)
+                    currentMaxValue = result[position];
+            }
+        }
+
+        for(int q = 0; q < result.length; q++){
+            if(result[q] < lastUpdate){
+                result[q] = lastUpdate;
             }
         }
 
